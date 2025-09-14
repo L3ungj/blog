69222ff31ab6eece2f204d87d3ae46a566e3ac69
L3ungj <justin.leungj@gmail.com>
2025-09-14 17:04:31 +0100

    Added ftp-deploy
    For website deployment to a domain name.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import PillNav from '@/ReactBits/PillNav/PillNav';
 export default function Header() {
   return (
     <>
-      <div className={`flex justify-center top-0 left-0 w-full z-50 shadow py-3 fixed`}>
+      <div className="flex justify-center top-0 left-0 w-full z-50 shadow py-3 fixed">
         <PillNav
           logo="/blog/assets/me.png"
           items={[
